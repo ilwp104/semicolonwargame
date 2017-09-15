0edeffc1c487f642c4dc30f90a3045eff7d32fa4
ilwp104 <will4981@naver.com>
2017-09-15 23:46:07 +0900

    메뉴 링크 추가, login & signup 추가

--- index.php
+++ index.php
@@ -4,74 +4,125 @@
     <meta charset="utf-8">
     <title>Wargame</title>
     <link rel="stylesheet" href="./css/index.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
   </head>
   <body>
-
-
-  <header>
-    <p style="font-size : 20px; margin : 0">Semecolon Wargame Site</p>
-    <nav class="sign">
-      <a class="" href="">Login</a>
-      <a class="" href="">Signup</a>
-    </nav>
-  </header>
-
-  <article class="article">
-    <nav class="MenuNav">
-      <span class="header">MENU</span>
-      <div class="wrapmenu">
-        <p class="" onclick="location.href='http://www.naver.com';">Login</p>
-        <p class="" onclick="location.href='';">Signup</p>
-        <p class="" onclick="location.href='';">main</p>
-        <p class="" onclick="location.href='';">Problem</p>
-      </div>
-    </nav>
-
-  <main>
-    <p>Welcome to Semecolon Wargame Site</p>
-    <div class="WrapContent">
-      <div class="notice">
-        <div class="NoticeHeader">
-          <p>Notice</p>
-        </div>
-        <div class="NoticeArticle">
-          <?php
-          //DB chat 내용 넣기
-          echo "<p></p>";
-          ?>
-        </div>
-      </div>
-
-      <div class="chat">
-        <div class="ChatHeader">
-          <p>Chat</p>
-        </div>
-        <div class="ChatArticle">
-          <?php
-          //DB연결하기
-          //DB chat 내용 넣기
-          echo "<p></p>";
-          ?>
-        </div>
-        <div class="ChatFooter">
-          <form class="chatform" action="from.php" method="get" onsubmit="">
-            <input type="text" class="ChatText" name="ChatText" placeholder="입력해주세요">
-            <input type="submit" value="Send" onclick="checkfuc();">
-          </form>
-        </div>
-      </div>
-    </div>
-  </main>
-</article>
-
-  <script type="text/javascript">
-    function checkfuc() {
-      if($(".ChatText").text()==""){
-        return false;
+    <script type="text/javascript">
+    $(function (){
+      $(".loginHeader p:nth-child(1)").css("background-color","#c8c8c8");
+    });
+      function changelogin(){
+          $(".loginArticle").css("display","block");
+          $(".signupArticle").css("display","none");
+          $(".loginHeader p:nth-child(1)").css("background-color","#c8c8c8");
+          $(".loginHeader p:nth-child(2)").css("background-color","#f1f1f1");
       }
-    }
-  </script>
+      function changesignup(){
+          $(".loginArticle").css("display","none");
+          $(".signupArticle").css("display","block");
+          $(".loginHeader p:nth-child(1)").css("background-color","#f1f1f1");
+          $(".loginHeader p:nth-child(2)").css("background-color","#c8c8c8");
+      }
+    </script>
+
+      <header>
+        <p style="font-size : 20px; margin : 0">Semecolon Wargame Site</p>
+      </header>
+
+      <article class="article">
+        <nav class="MenuNav">
+          <div class="Nav_icon">
+            <img src="./img/semicolon_logo.png" class="semiicon" alt="세미 로고">
+          </div>
+          <div class="wrapNavMenu">
+            <p class="" onclick="location.href='index.php';">Main</p>
+            <p class="" onclick="location.href='problem.php';">Problem</p>
+            <p class="" onclick="location.href='flag.php';">Flag</p>
+            <p class="" onclick="location.href='http://semi2012.godohosting.com/';">Semicolon Page</p>
+          </div>
+        </nav>
+
+      <main>
+        <p>Welcome to Semecolon Wargame Site</p>
+        <div class="WrapContent">
+          <div class="notice">
+            <div class="NoticeHeader">
+              <p>Notice</p>
+            </div>
+            <div class="NoticeArticle">
+              <?php
+              //DB chat 내용 넣기
+              echo "<p></p>";
+              ?>
+            </div>
+          </div>
+
+          <div class="login">
+            <div class="loginHeader">
+              <p onclick="changelogin();">Login</p>
+              <p onclick="changesignup();">SignUp</p>
+            </div>
+
+            <div class="loginArticle">
+              <form class="loginform" action="login.php" method="post">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                 <input class="mdl-textfield__input" name="id" type="text" id="sample3">
+                 <label class="mdl-textfield__label" for="sample3">Id</label>
+               </div>
+
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                  <input class="mdl-textfield__input" name="password" type="text" id="sample3">
+                  <label class="mdl-textfield__label" for="sample3">Password</label>
+                </div>
+
+                <input class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit" value="Login">
+              </form>
+            </div>
+
+            <div class="signupArticle">
+              <form class="signupform" action="signup.php" method="post">
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                  <input class="mdl-textfield__input" name="nickname" type="text" id="sample3">
+                  <label class="mdl-textfield__label" for="sample3">닉네임</label>
+                </div>
+
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                 <input class="mdl-textfield__input" name="id" type="text" id="sample3">
+                 <label class="mdl-textfield__label" for="sample3">아이디</label>
+                </div>
+
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                  <input class="mdl-textfield__input" name="password" type="text" id="sample3">
+                  <label class="mdl-textfield__label" for="sample3">비밀번호</label>
+                </div>
+
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                  <input class="mdl-textfield__input" name="name" type="text" id="sample3">
+                  <label class="mdl-textfield__label" for="sample3">이름</label>
+                </div>
+
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                  <input class="mdl-textfield__input" name="phonenum" type="text" id="sample3">
+                  <label class="mdl-textfield__label" for="sample3">전화번호</label>
+                </div>
+
+                <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                  <input class="mdl-textfield__input" name="shool" type="text" id="sample3">
+                  <label class="mdl-textfield__label" for="sample3">소속학교</label>
+                </div>
+
+                <input class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" type="submit" value="Signup">
+              </form>
+            </div>
+
+          </div>
+        </div>
+      </main>
+    </article>
   </body>
 </html>
