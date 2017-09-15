8a8f287bd0d990db4f9a854416fa03fca3447a2b
ilwp104 <will4981@naver.com>
2017-09-15 23:46:42 +0900

    problem 파일 생성

---
+++ problem.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Wargame</title>
+    <link rel="stylesheet" href="./css/index.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  </head>
+  <body>
+    <script type="text/javascript">
+
+    </script>
+
+      <header>
+        <p style="font-size : 20px; margin : 0">Semecolon Wargame Site</p>
+      </header>
+
+      <article class="article">
+        <nav class="MenuNav">
+          <div class="Nav_icon">
+            <img src="./img/semicolon_logo.png" class="semiicon" alt="세미 로고">
+          </div>
+          <div class="wrapNavMenu">
+            <p class="" onclick="location.href='index.php';">Main</p>
+            <p class="" onclick="location.href='problem.php';">Problem</p>
+            <p class="" onclick="location.href='flag.php';">Flag</p>
+            <p class="" onclick="location.href='http://semi2012.godohosting.com/';">Semicolon Page</p>
+          </div>
+        </nav>
+
+      <main>
+        <p>Problem</p>
+        
+      </main>
+    </article>
+  </body>
+</html>
