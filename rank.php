37d443e4befb7a8d53e9e5b6966996bc61982bfb
ilwp104 <will4981@naver.com>
2017-09-18 01:13:32 +0900

    rank 페이지 추가

---
+++ rank.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Wargame</title>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  </head>
+  <body>
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/rank.css">
+    <main>
+      <div class="head">
+        <p>Ranking</p>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <td>Rarking</td>
+            <td>Nickname</td>
+            <td>Score</td>
+            <td>School</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>admin</td>
+            <td>100</td>
+            <td>한세</td>
+          </tr>
+        </tbody>
+
+      </table>
+    </main>
+  </body>
+</html>
