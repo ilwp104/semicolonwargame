4a1f4d47807a7b4d11bd50e8606b7469fd082094
ilwp104 <will4981@naver.com>
2017-09-17 22:22:04 +0900

    problem 페이지 추가

--- problem.php
+++ problem.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <title>Wargame</title>
     <link rel="stylesheet" href="./css/index.css">
-    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/problem.css">
 
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
